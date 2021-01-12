18f085831abd36e47df117907ea107482d6849a5
averyanalex <averyanalex@gmail.com>
2021-01-12 19:40:39 +0300

    Config refactor

--- utils/config.go
+++ utils/config.go
@@ -9,20 +9,32 @@ import (
 
 // ReadConfig will read configs and env to viper
 func ReadConfig() {
-	var err error
+	// var err error
 
-	viper.SetConfigFile("base.env")
-	viper.SetConfigType("yaml")
-	err = viper.ReadInConfig()
-	if err != nil {
-		fmt.Println(err)
-		return
+	// viper.SetConfigFile("base.env")
+	// viper.SetConfigType("yaml")
+	// err = viper.ReadInConfig()
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+
+	if _, err := os.Stat("/etc/paper-chat/api.yml"); os.IsNotExist(err) {
+		fmt.Println("Config file /etc/paper-chat/api.yml not found")
+	} else {
+		viper.SetConfigFile("/etc/paper-chat/api.yml")
+		viper.SetConfigType("yaml")
+		err = viper.ReadInConfig()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
 	}
 
-	if _, err := os.Stat(".env"); os.IsNotExist(err) {
-		fmt.Println("WARNING: file .env not found")
+	if _, err := os.Stat("~/.config/paper-chat/api.yml"); os.IsNotExist(err) {
+		fmt.Println("Config file ~/.config/paper-chat/api.yml not found")
 	} else {
-		viper.SetConfigFile(".env")
+		viper.SetConfigFile("~/.config/paper-chat/api.yml")
 		viper.SetConfigType("yaml")
 		err = viper.MergeInConfig()
 		if err != nil {
@@ -31,10 +43,10 @@ func ReadConfig() {
 		}
 	}
 
-	if _, err := os.Stat("/etc/nnm/base.env"); os.IsNotExist(err) {
-		fmt.Println("WARNING: file base.env not found")
+	if _, err := os.Stat("api.yml"); os.IsNotExist(err) {
+		fmt.Println("Config file api.yml not found")
 	} else {
-		viper.SetConfigFile("/etc/nnm/base.env")
+		viper.SetConfigFile("api.yml")
 		viper.SetConfigType("yaml")
 		err = viper.MergeInConfig()
 		if err != nil {
