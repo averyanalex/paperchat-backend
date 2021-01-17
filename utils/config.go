0024eb0d6552750e62a68f8423765a720cffaa2c
averyanalex <averyanalex@gmail.com>
2021-01-17 12:55:19 +0300

    Config

--- utils/config.go
+++ utils/config.go
@@ -2,61 +2,25 @@ package utils
 
 import (
 	"fmt"
-	"os"
+	"strings"
 
 	"github.com/spf13/viper"
 )
 
 // ReadConfig will read configs and env to viper
 func ReadConfig() {
-	// var err error
-
-	// viper.SetConfigFile("base.env")
-	// viper.SetConfigType("yaml")
-	// err = viper.ReadInConfig()
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	return
-	// }
-
-	if _, err := os.Stat("/etc/paper-chat/api.yml"); os.IsNotExist(err) {
-		fmt.Println("Config file /etc/paper-chat/api.yml not found")
-	} else {
-		viper.SetConfigFile("/etc/paper-chat/api.yml")
-		viper.SetConfigType("yaml")
-		err = viper.ReadInConfig()
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
+	viper.SetConfigName("api.yml")                  // name of config file (without extension)
+	viper.SetConfigType("yaml")                     // REQUIRED if the config file does not have the extension in the name
+	viper.AddConfigPath("/etc/paper-chat/")         // path to look for the config file in
+	viper.AddConfigPath("$HOME/.config/paper-chat") // call multiple times to add many search paths
+	viper.AddConfigPath(".")                        // optionally look for config in the working directory
+	err := viper.ReadInConfig()                     // Find and read the config file
+	if err != nil {                                 // Handle errors reading the config file
+		panic(fmt.Errorf("Fatal error config file: %s \n", err))
 	}
 
-	if _, err := os.Stat("~/.config/paper-chat/api.yml"); os.IsNotExist(err) {
-		fmt.Println("Config file ~/.config/paper-chat/api.yml not found")
-	} else {
-		viper.SetConfigFile("~/.config/paper-chat/api.yml")
-		viper.SetConfigType("yaml")
-		err = viper.MergeInConfig()
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-	}
-
-	if _, err := os.Stat("api.yml"); os.IsNotExist(err) {
-		fmt.Println("Config file api.yml not found")
-	} else {
-		viper.SetConfigFile("api.yml")
-		viper.SetConfigType("yaml")
-		err = viper.MergeInConfig()
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-	}
-
-	// Override config parameters from environment variables if specified
-	for _, key := range viper.AllKeys() {
-		viper.BindEnv(key)
-	}
+	replacer := strings.NewReplacer(".", "_")
+	viper.SetEnvKeyReplacer(replacer)
+	viper.SetEnvPrefix("paper")
+	viper.AutomaticEnv()
 }
